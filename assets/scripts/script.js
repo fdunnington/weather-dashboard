00064479caa01a9b1f71a5bcbf3404ff902dd284
Fi Dunnington <fi.dunnington@gmail.com>
2023-02-06 18:48:31 +0100

    Added forecast cards. Broke 'TodaysData()' which no longer pulls date. To be fixed

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,58 +1,116 @@
-//TODO
-//1. add onLoad
-//2. add AJAX get request
-//   api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
-//add apiKey const = "82b225f1a74acfbe188e326f3d399e83";
-
-//JS elements
-// > search input
-// > submit button
-// > current date & time
-// > current temp
-// > current humidity
-
-// > 5-day weather card
 
 $(document).ready(function() {
 
-
-
-    //page loads previous searches
-    function getHistory (){
+    //function to show saved city button after refresh
+    function showSavedData() {
 
     }
 
     //-------------------------------------------------    
     //event listener for submit button
     //-------------------------------------------------
+  
+   
     $("#search-button").on("click", function(event){
-
         event.preventDefault();
+        todaysData();
+        getForecast();
+        //saveCity();
+    }); 
 
-        const queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=";
-        var city = $("#search-input").val().trim();
+    const queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=";
+    const apiKey = "82b225f1a74acfbe188e326f3d399e83";
+    const cityID = $("#search-input").val().trim();
 
-        console.log(city);            
-        
+    const tempID = $(".temp");
+    const windData = $(".wind");
+    const humidity = $(".humidity");
+    const weatherIcon = $("#weather-image");
+    const today = moment().format("ddd DD MMM YYYY");
+    const forecast = $("#forecast");
+
+
+
+
+    //-------------------------------------------------
+    //pull and display todays data in #today
+    //-------------------------------------------------
+    function todaysData() {
+
+
         $.ajax({
-            url: queryURL + city + "&appid=82b225f1a74acfbe188e326f3d399e83&units=metric", 
+            url: queryURL + cityID + "&appid=" + apiKey + "&units=metric", 
             method: "GET"
         }).then(function(data) {
-            console.log(data);
+            console.log(data);  
+            let i = 4;
+            
+            $("#chosen-location").text(chosenLocation + ": ");
+            $("#todays-date").text(today);
+            $("#current-temp").text((data.list[i].main.temp).toFixed(0) + "°C");
+            $("#current-wind").text((Number(data.list[i].wind.speed) * 1.94384).toFixed(2) + "kts");
+            $("#current-humidity").text(data.list[i].main.humidity  + "%");
 
-            //-------------------------------------------------
-            //pull and display todays data in #today
-            //-------------------------------------------------
-            const today = moment().format("ddd DDD MMM YYYY");
-            $("#chosen-location").text(data.city.name + " (" + today + ") ");
-            $("#current-temp").text(data.list['0'].main.temp + "°C");
-            $("#current-wind").text(data.list['0'].wind.speed + "mps");
-            $("#current-humidity").text(data.list['0'].main.humidity  + "%");
-
-
+            
         });
+    };
 
-        
-    });
+    //-------------------------------------------------
+    //pull and display data in #forecast
+    //-------------------------------------------------
+    function getForecast() {
+        //clear for new search result
+    $("#weather-forecast").html("");
+    $.ajax({
+        url: queryURL + cityID + "&appid=" + apiKey + "&units=metric", 
+        method: "GET"
+    }).then(function(data) {
+
+
+            var forecastData = data.list;
+
+
+            //divide by 8 since API updates weather every 3 hours a day
+            for (var i = 1; i <= forecastData.length / 8; i++) {
+
+                var getIcon = forecastData[i * 7].weather[0].icon;
+
+                //get epoch time and convert to date
+                var getForDate = forecastData[i * 4].dt * 1000;
+                var getWeatherDate = new Date(getForDate).getDate();
+                var getWeatherMonth = new Date(getForDate).getMonth();
+                var getWeatherYear = new Date(getForDate).getFullYear();
+
+                var getForTemp = forecastData[i * 4].main.temp;
+                var getForHum = forecastData[i * 4].main.humidity;
+
+
+                //create card body
+                var weatherCard = $('<div>').attr({ "class": "card bg-info shadow m-4 flex-container" });
+
+                var weatherForecast = $('<div>').attr({ "class": "card-body" });
+                var iconURL = $('<img>').attr({ "src": "https://openweathermap.org/img/w/" + getIcon + ".png" });
+
+                var weatherForDate = $('<p>').html(getWeatherMonth + "/" + getWeatherDate + "/" + getWeatherYear);
+
+
+                var weatherIcon = $("<p>").append(iconURL);
+
+                var forecastTemp = $('<p>').text("Temp: " + (data.list[i].main.temp).toFixed(0) + "°C");
+                var forecastWind = $('<p>').text("Wind: " + (Number(data.list[i].wind.speed) * 1.94384).toFixed(2) + "kts");
+                var forecastHumidity = $('<p>').text("Humidity: " + data.list[i].main.humidity  + "%");
+
+
+                weatherForecast.append(weatherForDate, weatherIcon, forecastWind, forecastTemp, forecastHumidity);
+
+                weatherCard.append(weatherForecast);
+                $("#weather-forecast").append(weatherCard);
+
+            }
+
+
+        })
+    };
+
 
 });
